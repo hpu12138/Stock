c12ee62a2b67646ff348bd1da6c02d9e528ad9f0
rabbitandcat <phmhappier@163.com>
2017-11-01 12:23:30 +0800

    Add files via upload

---
+++ R/StockPredictoR.R
@@ -0,0 +1,41 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+#' @title Predicts Stock PriceMovement for Given Stock Symbol
+
+#'
+
+#' @deion This package predictswhether the stock price at tommorow's market close would be higher or lowercompared to today's closing place.
+
+#'
+
+#' @param symbol
+
+#'
+
+#' @return NULL
+
+#'
+
+#' @examples stock_predict('AAPL')
+
+#'
+
+#' @export stock_predict
+
+stock_predict<-function(symbol)
+
+{
+  print(symbol)
+}
